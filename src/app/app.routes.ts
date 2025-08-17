2f2b03d76ddac2791e2ea7e8e31dfe7056600d4c
Marcos Prado <marcospradofelix@gmail.com>
2025-08-16 23:41:59 -0300

    feat: add route for client registration component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import {ClientRegister} from './components/client-register/client-register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'client/register',
+    component: ClientRegister
+  }
+];
