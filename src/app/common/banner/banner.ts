6ba701e132518dbb9329ff1ee0dc00e73a5ef131
Marcos Prado <marcospradofelix@gmail.com>
2025-08-23 15:48:12 -0300

    feat: add Banner component with template, styles, and tests

---
+++ src/app/common/banner/banner.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-banner',
+  imports: [],
+  templateUrl: './banner.html',
+  styleUrl: './banner.css'
+})
+export class Banner {
+
+}
