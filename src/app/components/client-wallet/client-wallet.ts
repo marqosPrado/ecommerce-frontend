608419b4dcdcfa0122fd0a8c8b406ac5ad07eccd
Marcos Prado <marcospradofelix@gmail.com>
2025-08-22 23:07:04 -0300

    feat: enhance client wallet component with credit card form and reactive forms

--- src/app/components/client-wallet/client-wallet.ts
+++ src/app/components/client-wallet/client-wallet.ts
@@ -1,18 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {Header} from '../../common/header/header';
 import {Button} from 'primeng/button';
 import {Card} from 'primeng/card';
+import {FloatLabel} from 'primeng/floatlabel';
+import {InputText} from 'primeng/inputtext';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {InputMask} from 'primeng/inputmask';
+import {DatePicker} from 'primeng/datepicker';
+import {Checkbox} from 'primeng/checkbox';
 
 @Component({
   selector: 'app-client-wallet',
   imports: [
     Header,
     Button,
-    Card
+    Card,
+    FloatLabel,
+    InputText,
+    ReactiveFormsModule,
+    InputMask,
+    DatePicker,
+    Checkbox
   ],
   templateUrl: './client-wallet.html',
   styleUrl: './client-wallet.css'
 })
 export class ClientWallet {
+  showCreditCardForm = signal(false)
 
+  handleCreditCardForm() {
+    this.showCreditCardForm.set(!this.showCreditCardForm());
+  }
+
+  creditCardForm = new FormGroup({
+    mainCard: new FormControl<boolean>(false, { nonNullable: true })
+  })
 }
