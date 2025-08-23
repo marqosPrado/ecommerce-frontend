6266e938c0436fc10f625bc2c9a8e01dd1addb1c
Marcos Prado <marcospradofelix@gmail.com>
2025-08-22 22:22:20 -0300

    feat: add route for client wallet component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import {ClientRegister} from './components/client-register/client-register';
 import {ListClients} from './components/list-clients/list-clients';
 import {EditClient} from './components/edit-client/edit-client';
+import {ClientWallet} from './components/client-wallet/client-wallet';
 
 export const routes: Routes = [
   {
@@ -15,5 +16,9 @@ export const routes: Routes = [
   {
     path: 'client/edit/:id',
     component: EditClient
+  },
+  {
+    path: 'minha-conta/carteira',
+    component: ClientWallet
   }
 ];
