bcbe3f3f67ad4fda898626b2ab7a4dce2b93800b
Marcos Prado <marcospradofelix@gmail.com>
2025-08-23 17:11:58 -0300

    feat: rename Watch component to Product and update type definition

--- src/app/types/Product.ts
+++ src/app/types/Product.ts
@@ -1,4 +1,4 @@
-type Watch = {
+type Product = {
   id: number;
   title: string;
   price: number;
@@ -9,4 +9,5 @@ type Watch = {
   boxMaterial: string;
   boxFormat: string;
   dial: string;
+  images: string[];
 }
