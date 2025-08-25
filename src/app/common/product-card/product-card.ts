46ead1e7f8130672cc0259c6d515817b2f6bbae9
Marcos Prado <marcospradofelix@gmail.com>
2025-08-24 22:24:46 -0300

    feat: adiciona router para página do botão

--- src/app/common/product-card/product-card.ts
+++ src/app/common/product-card/product-card.ts
@@ -1,15 +1,17 @@
-import {Component, Input} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Button } from 'primeng/button';
-import {DecimalPipe} from '@angular/common';
+import { DecimalPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-card',
   standalone: true,
-  imports: [Button, DecimalPipe],
+  imports: [Button, DecimalPipe, RouterLink],
   templateUrl: './product-card.html',
   styleUrls: ['./product-card.css']
 })
 export class ProductCard {
+  @Input() id!: number;
   @Input() title!: string;
   @Input() price!: number;
 }
