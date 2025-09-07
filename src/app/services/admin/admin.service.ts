38c47d419de6c7ce4323d98f94dc9e00083be576
Marcos Prado <marcospradofelix@gmail.com>
2025-09-06 21:39:24 -0300

    feat: implement admin service

---
+++ src/app/services/admin/admin.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpParams} from '@angular/common/http';
+import {Gender} from '../../types/Client';
+import {Page} from '../../types/Page';
+import {Observable} from 'rxjs';
+
+
+export type ClientFilter = {
+  fullName: string;
+  cpf: string;
+  email: string;
+  phoneNumber: string;
+  gender: Gender;
+  active: boolean;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminService {
+
+  private baseUrl: string = 'http://localhost:8080/admin/client';
+
+  constructor(
+    private http: HttpClient,
+  ) {
+  }
+
+  getClients(filter: Partial<ClientFilter>, page: number, size: number): Observable<Page<ClientFilter>> {
+    const url = `${this.baseUrl}/search`;
+    let params = new HttpParams()
+      .set('page', page.toString())
+      .set('size', size.toString());
+
+    Object.keys(filter).forEach((key) => {
+      const k = key as keyof ClientFilter;
+      const value = filter[k];
+
+      if (value !== null && value !== '' && value !== undefined) {
+        params = params.set(k, value.toString());
+      }
+    });
+
+    return this.http.get<Page<ClientFilter>>(url, { params });
+  }
+}
