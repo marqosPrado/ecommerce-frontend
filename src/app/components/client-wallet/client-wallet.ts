c26993a1077d841911da594b45ba62626702311b
Marcos Prado <marcospradofelix@gmail.com>
2025-09-07 13:25:42 -0300

    feat: implement functionality to register new credit card

--- src/app/components/client-wallet/client-wallet.ts
+++ src/app/components/client-wallet/client-wallet.ts
@@ -8,6 +8,11 @@ import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 import {InputMask} from 'primeng/inputmask';
 import {DatePicker} from 'primeng/datepicker';
 import {Checkbox} from 'primeng/checkbox';
+import {Toast} from 'primeng/toast';
+import {ClientService} from '../../services/client.service';
+import {MessageService} from 'primeng/api';
+import {CreditCardTypes} from '../../types/CreditCard';
+import {Select} from 'primeng/select';
 
 @Component({
   selector: 'app-client-wallet',
@@ -20,24 +25,51 @@ import {Checkbox} from 'primeng/checkbox';
     ReactiveFormsModule,
     InputMask,
     DatePicker,
-    Checkbox
+    Checkbox,
+    Toast,
+    Select
   ],
   templateUrl: './client-wallet.html',
   styleUrl: './client-wallet.css'
 })
 export class ClientWallet {
   showCreditCardForm = signal(false)
+  creditCardForm: FormGroup;
 
-  creditCardForm = new FormGroup({
-    name: new FormControl('', Validators.required),
-    printedName: new FormControl('', Validators.required),
-    validity: new FormControl('', Validators.required),
-    verificationCode: new FormControl('', Validators.required),
-    cpf: new FormControl('', Validators.required),
-    birthDate: new FormControl('', Validators.required),
-    surname: new FormControl('', Validators.required),
-    mainCard: new FormControl<boolean>(false, { nonNullable: true })
-  })
+  constructor(
+    private clientService: ClientService,
+    private messageService: MessageService
+  ) {
+    this.creditCardForm = new FormGroup({
+      number: new FormControl('', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]),
+      printedName: new FormControl('', Validators.required),
+      cardFlag: new FormControl<CreditCardTypes | null>(null, Validators.required),
+      cpf: new FormControl('', Validators.required),
+      birthDate: new FormControl('', Validators.required),
+      surname: new FormControl('', Validators.required),
+      securityCode: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(4)]),
+      isMain: new FormControl<boolean>(false, { nonNullable: true })
+    });
+  }
+
+  submitCreditCard() {
+    if (this.creditCardForm.invalid) return;
+
+    const clientId = 1;
+    const cardData = this.creditCardForm.value;
+
+    this.clientService.registerNewCreditCard(clientId, cardData).subscribe({
+      next: (res) => {
+        this.showCreditCardForm.set(false);
+        this.creditCardForm.reset({ isMain: false });
+        this.messageService.add({severity:'success', summary:'Sucesso', detail:'Cartão cadastrado com sucesso!'});
+      },
+      error: (err) => {
+        console.error('Erro ao cadastrar cartão', err);
+        this.messageService.add({severity:'error', summary:'Erro', detail:'Não foi possível cadastrar o cartão.'});
+      }
+    });
+  }
 
   handleCreditCardForm() {
     this.showCreditCardForm.set(!this.showCreditCardForm());
