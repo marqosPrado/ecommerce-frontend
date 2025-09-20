e93995a68e099e0aa59880dcd4da0422dc0f0822
Marcos Prado <marcospradofelix@gmail.com>
2025-09-20 20:38:49 -0300

    feat: create selector to basic client registration data

---
+++ cypress/support/selectors/client-register-selectors.ts
@@ -0,0 +1,12 @@
+export const registerClientSelectors = {
+  fullName: '[data-cy="input-fullName"]',
+  cpf: '[data-cy="input-cpf"]',
+  gender: '[data-cy="select-gender"]',
+  birthDate: '[data-cy="input-birthDate"]',
+  typePhoneNumber: '[data-cy="select-typePhoneNumber"]',
+  phoneNumber: '[data-cy="input-phoneNumber"]',
+  email: '[data-cy="input-email"]',
+  password: '[data-cy="input-password"]',
+  confirmPassword: '[data-cy="input-confirmPassword"]',
+  btnNextStep: '[data-cy="btn-step1-next"] button',
+}
